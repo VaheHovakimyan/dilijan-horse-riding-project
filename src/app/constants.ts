89691df6c42d2a339f46aed9a8ce2962cbb2567d
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2024-11-03 22:10:07 +0400

    delete slogan component, add video welcome component

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,3 @@
-import image from './_lib/common/ui/assets/images/horse_image.jpg';
-
 export const BASE_API_URL =  "http://127.0.0.1:1337/api";
 export const AUTH_TOKEN =  "e1b4ad01b4bcff4750a619f88306e60299cc2db291ff9e88eff48f63b93f69b0f619bf7a697422766367d8528f9006caa142f7ef64edbf8d779948bd59096d53a60e70aae3c4bc2ecb6570d371e72340e5f0677daaf46ce9cce9ef60a7decf4975a2aa02fad338e292a3e6687c2951a86b5778e475d8121fa11144628b02fe51";
 export const description = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptate quam tenetur laudantium vel omnis pariatur quia rerum odio. Nobis ad, iusto qui impedit ratione accusantium at doloremque in saepe!
