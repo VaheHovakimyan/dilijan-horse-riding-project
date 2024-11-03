a973b28bf52ae17ad84a5a12ae37568a7ac7b6f4
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2024-11-03 22:34:21 +0400

    fix gallery error

--- src/app/_lib/common/ui/pages/HomePageNew/galleryHome/GalleryHome.tsx
+++ src/app/_lib/common/ui/pages/HomePageNew/galleryHome/GalleryHome.tsx
@@ -1,43 +1,40 @@
-'use client'
-
-import React from "react";
 import style from "./GalleryHome.module.scss";
 import mediaStyle from './GalleryHomeMedia.module.scss';
-import Link from "next/link";
+import NavigationLink from "@/app/_components/NavigationLink";
 
 const arr: number[] = [0, 1, 2];
 
 export const GalleryHome = ({ }: {}) => {
 
   return (
-    <Link href={"/gallery"}>
-    <div className={`${style.gallery_home_main} ${mediaStyle.gallery_home_main}`}>
-      <div className={`${style.gallery_home} ${mediaStyle.gallery_home}`}>
+    <NavigationLink href={"/gallery"}>
+      <div className={`${style.gallery_home_main} ${mediaStyle.gallery_home_main}`}>
+        <div className={`${style.gallery_home} ${mediaStyle.gallery_home}`}>
 
-        <div className={`${style.gallery_home_main_1} ${mediaStyle.gallery_home_main_1}`}>
-          {arr.map((item: number) => {
-            return (
-              <div className={`${style.gallery_home_main_item} ${mediaStyle.gallery_home_main_item}`} key={item}>
-                <div className={style.gallery_home_main_item_opacity}></div>
-              </div>
-            )
-          })}
-        </div>
-
-        <div className={`${style.gallery_home_main_2} ${mediaStyle.gallery_home_main_2}`}>
-          {arr.map((item: number) => {
-            return (
-              <div className={`${style.gallery_home_main_item} ${mediaStyle.gallery_home_main_item}`} key={item}>
-                <div className={`${style.gallery_home_main_item_opacity} ${mediaStyle.gallery_home_main_item_opacity}`}>
-                  {item === arr.length - 1 && <p className={`${style.gallery_home_main_item_opacity_last} ${mediaStyle.gallery_home_main_item_opacity_last}`}>+ 343</p>}
+          <div className={`${style.gallery_home_main_1} ${mediaStyle.gallery_home_main_1}`}>
+            {arr.map((item: number) => {
+              return (
+                <div className={`${style.gallery_home_main_item} ${mediaStyle.gallery_home_main_item}`} key={item}>
+                  <div className={style.gallery_home_main_item_opacity}></div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
+
+          <div className={`${style.gallery_home_main_2} ${mediaStyle.gallery_home_main_2}`}>
+            {arr.map((item: number) => {
+              return (
+                <div className={`${style.gallery_home_main_item} ${mediaStyle.gallery_home_main_item}`} key={item}>
+                  <div className={`${style.gallery_home_main_item_opacity} ${mediaStyle.gallery_home_main_item_opacity}`}>
+                    {item === arr.length - 1 && <p className={`${style.gallery_home_main_item_opacity_last} ${mediaStyle.gallery_home_main_item_opacity_last}`}>+ 343</p>}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
-    </div>
-    </Link>
+    </NavigationLink>
   );
 };
 
