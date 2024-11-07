c91bff094ab61e1e03ece24ff6f566828937ceb9
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2024-11-07 21:05:50 +0400

    make footer multilanguage

--- src/app/_lib/common/ui/shared/Footer/Footer.tsx
+++ src/app/_lib/common/ui/shared/Footer/Footer.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from "react";
 import Image from "next/image";
 
@@ -10,8 +8,12 @@ import footer_logo from '../../assets/images/header_logo_.jpg';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TelegramIcon from '@mui/icons-material/Telegram';
+import { useTranslations } from "next-intl";
 
 export const Footer = ({ }: {}) => {
+
+  const t = useTranslations("Footer");
+
 
   return (
     <div className={`${style.footer_main} ${mediaStyle.footer_main}`}>
@@ -26,9 +28,9 @@ export const Footer = ({ }: {}) => {
           />
           <div className={`${style.footer_social_div} ${mediaStyle.footer_social_div}`}>
             <div className={`${style.footer_social_contacts_div} ${mediaStyle.footer_social_contacts_div}`}>
-              <p className={`${style.footer_social_contact} ${mediaStyle.footer_social_contact}`}>Telephone: (+374)77-97-73-34</p>
-              <p className={`${style.footer_social_contact} ${mediaStyle.footer_social_contact}`}>Address: Dilijan, Village of Haghartsin</p>
-              <p className={`${style.footer_social_contact} ${mediaStyle.footer_social_contact}`}>Email: dilijanhorseriding@gmail.com</p>
+              <p className={`${style.footer_social_contact} ${mediaStyle.footer_social_contact}`}>{t('contacts.telephone')}</p>
+              <p className={`${style.footer_social_contact} ${mediaStyle.footer_social_contact}`}>{t('contacts.address')}</p>
+              <p className={`${style.footer_social_contact} ${mediaStyle.footer_social_contact}`}>{t('contacts.email')}</p>
             </div>
             <div className={`${style.footer_social_icons_div} ${mediaStyle.footer_social_icons_div}`}>
               <a href="https://www.facebook.com/dilijanhorseriding/" target="_blank" className={style.footer_social_icon_link}>
@@ -44,25 +46,20 @@ export const Footer = ({ }: {}) => {
           </div>
         </div>
         <div className={`${style.footer_mid_div_flex} ${mediaStyle.footer_mid_div_flex}`}>
-          <h3 className={`${style.footer_mid_div_title} ${mediaStyle.footer_mid_div_title}`}>Come feel the breath of the mountains</h3>
+          <h3 className={`${style.footer_mid_div_title} ${mediaStyle.footer_mid_div_title}`}>{t('FooterDescription.title')}</h3>
           <div>
             <p className={`${style.footer_mid_div_text} ${mediaStyle.footer_mid_div_text}`}>
-              Dilijan Horseriding-ը պրոեկտ է, որով ապրում ու զարգանում
-              է գյուղը, ի շնորհիվ նախաձեռնության տուրիզմի շատ
-              ուղղություններ Տավուշի մարզում դարձան էլ ավելի
-              ճանաչելի, բացվեցին մի շարք հյուրատներ ու մարդիկ սկսեցին
-              մտածել հայրենի բնաշխարհում նոր գաղափարներ արարելու մասին:
-              Մեր գլխավոր առաքելությունն է մեզ այցելող հյուրերի մեջ սերմանել
-              սերը դեպի բնությունը, լեռներն ու կենդանիները, և դա է պատճառը,
-              որ մեր 1000 ավոր հյուրեր գալով մեզ մոտ միշտ գտնում ու իրենց հետ տանում են՝
+              {t('FooterDescription.description')}
             </p>
             <p className={`${style.footer_slogan} ${mediaStyle.footer_slogan}`}>
-              ՍԱՐԵՐ ՁՈՐԵՐ ՈՒ ԼԻ՜ ՔԸ ՍԵՐ
+              {t('FooterDescription.slogan')}
             </p>
           </div>
         </div>
         <div className={`${style.footer_map_div} ${mediaStyle.footer_map_div}`}>
-          <p className={`${style.footer_map_title} ${mediaStyle.footer_map_title}`}>We are on the map</p>
+          <p className={`${style.footer_map_title} ${mediaStyle.footer_map_title}`}>
+            {t('mapTitle')}
+          </p>
           <div className={`${style.footer_map} ${mediaStyle.footer_map}`}>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.415777499316!2d44.97570177550167!3d40.77487263376502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x404055567b9935e5%3A0x17606dbcb9a0e871!2sDilijan%20Horse%20riding!5e0!3m2!1sru!2sam!4v1720170929648!5m2!1sru!2sam" width={'100%'} height={'100%'} style={{ border: 0 }} loading="lazy"></iframe>
           </div>
