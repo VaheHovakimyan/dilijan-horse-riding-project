cee6102cf90b33f96a7b8e073aca3572a8e5fec9
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2024-11-11 10:37:42 +0400

    change home services, make  responsive authors component

--- src/app/_lib/common/ui/pages/HomePageNew/ourServices/OurServicesHome.tsx
+++ src/app/_lib/common/ui/pages/HomePageNew/ourServices/OurServicesHome.tsx
@@ -20,13 +20,19 @@ const OurServicesHome = () => {
       <p className={`${style.our_services_home_title} ${mediaStyle.our_services_home_title}`}>{t('title')}</p>
       <div className={`${style.our_services_home_flex} ${mediaStyle.our_services_home_flex}`}>
         {ourServices?.map((item: any, index: number) => {
-          
+
           return (
-            <div key={index} className={`${style.our_services_home_item} ${mediaStyle.our_services_home_item}`}>
+            <div
+              className={`${style.our_services_home_item} ${mediaStyle.our_services_home_item}`}
+              style={{ flexDirection: index % 2 === 0 ? 'row' : "row-reverse" }}
+              key={index}
+            >
               <div className={`${style.our_services_home_item_opacity} ${mediaStyle.our_services_home_item_opacity}`} style={{ backgroundImage: `url(${t(`${item}.image`)})` }} >
-                <div className={`${style.our_services_home_item_p_center} ${mediaStyle.our_services_home_item_p_center}`}>
-                  <p className={`${style.our_services_home_item_p} ${mediaStyle.our_services_home_item_p}`}>{t(`${item}.name`)}</p>
-                </div>
+                <div className={`${style.our_services_home_item_p_center} ${mediaStyle.our_services_home_item_p_center}`}></div>
+              </div>
+              <div className={`${style.our_services_home_item_description_div} ${mediaStyle.our_services_home_item_description_div}`}>
+                <h2 className={`${style.our_services_home_item_description_title} ${mediaStyle.our_services_home_item_description_title}`}>{t(`${item}.name`)}</h2>
+                <p className={`${style.our_services_home_item_description_p} ${mediaStyle.our_services_home_item_description_p}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id natus autem fugiat tenetur veritatis temporibus porro sunt quam, officiis quos ut aut ad ipsam reiciendis exercitationem numquam saepe facilis. Dolorum.</p>
               </div>
             </div>
           )
