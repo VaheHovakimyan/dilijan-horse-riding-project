bd61710ce0cdecfdad0ef768bc0179583ae43558
VaheHovakimyan <88284245+VaheHovakimyan@users.noreply.github.com>
2024-11-15 18:42:08 +0400

    Update WelcomeComponent.tsx

--- src/app/_lib/common/ui/pages/HomePageNew/welcomeComponent/WelcomeComponent.tsx
+++ src/app/_lib/common/ui/pages/HomePageNew/welcomeComponent/WelcomeComponent.tsx
@@ -25,6 +25,7 @@ const WelcomeComponent = () => {
         loop
         autoPlay
         muted
+        playsinline
         className={`${style.welcome_main_video} ${mediaStyle.welcome_main_video}`}
         >
         <track default kind="captions"/>
